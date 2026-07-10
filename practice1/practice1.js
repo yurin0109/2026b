21edf33fe1b96090e25e750936b7e96deef02cf6
yurin0109 <yurin0109>
2026-07-10 10:39:34 +0900

    0710

---
+++ practice1/practice1.js
@@ -0,0 +1,9 @@
+// 2. prompt (출력할메시지) : 브라우저 입력가능한 알림창 , 입력한 자료 변환
+let 이름 = prompt( "이름이 무엇인가요?")
+console.log ( 이름 )
+
+let 메뉴  = prompt( " 주문하실 메뉴가 어떻게 되시나요?")
+console.log ( 메뉴 )
+
+let 수량 = prompt( " 몇 개를 구매하실 건가요? ")
+console.log ( 수량 )
