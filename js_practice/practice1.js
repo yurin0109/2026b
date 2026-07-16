d9a2302c75517b81aa8e210817cdaf25b7682600
yurin0109 <yurin0109>
2026-07-16 14:07:09 +0900

    26.07.16

---
+++ js_practice/practice1.js
@@ -0,0 +1,9 @@
+let 이름 = prompt("이름이 무엇인가요?");
+console.log(이름);
+
+let 메뉴 = prompt("주문하실 메뉴가 어떻게 되시나요?");
+console.log(메뉴);
+
+let 수량 = Number(prompt("몇 개를 구매하실 건가요?"));
+console.log(수량);
+
--- js_practice/practice2.js
+++ js_practice/practice2.js
@@ -1,15 +1,3 @@
-// prompt 연습
-
-let 이름 = prompt("이름이 무엇인가요?");
-console.log(이름);
-
-let 메뉴 = prompt("주문하실 메뉴가 어떻게 되시나요?");
-console.log(메뉴);
-
-let 수량 = Number(prompt("몇 개를 구매하실 건가요?"));
-console.log(수량);
-
-
 // 문제 1
 
 let 밑변 = Number(prompt("밑변 : "));
